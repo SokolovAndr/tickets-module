4999f70106f89ddd9e85aeede3d1d9d26bb93a35
an.sokolov <andrei.sokolov@stm-labs.ru>
2026-06-02 16:28:19 +0300

    logging added

--- tickets-module-impl/src/main/java/com/example/ticketsmoduleimpl/carriers/resource/CarriersControllerApiImpl.java
+++ tickets-module-impl/src/main/java/com/example/ticketsmoduleimpl/carriers/resource/CarriersControllerApiImpl.java
@@ -27,39 +27,25 @@ public class CarriersControllerApiImpl implements CarriersControllerApi {
 
     @Override
     public ResponseEntity<CreateCarrierResponse> createCarrier(@RequestBody CreateCarrierRequest createCarrierRequest) {
-        log.debug("createCarrier response: {}", createCarrierRequest);
-
         CreateCarrierResponse result = carriersService.create(createCarrierRequest);
-        log.debug("createCarrier result: {}", result);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
     @Override
     public ResponseEntity<Page> getAllCarriers(@Valid Pageable pageable, @Valid CarriersSearchRequest searchParam) {
-        log.debug("getAllCarriers searchParam: {}, pageable: {}", searchParam, pageable);
-
         Page<CreateCarrierResponse> result = carriersService.findAll(searchParam, pageable);
-
-        log.debug("getAll result totalElements: {}", result.getTotalElements());
         return ResponseEntity.ok(result);
     }
 
     @Override
     public ResponseEntity<CreateCarrierResponse> getCarrier(@NotNull UUID id) {
-        log.debug("get carrier id: {}", id);
         CreateCarrierResponse result = carriersService.findOne(id);
-        log.debug("get carrier result: {}", result);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @Override
     public ResponseEntity<CreateCarrierResponse> patch(@NotNull UUID id, @Valid CarrierPatchRequest carrierPatchRequest) {
-        log.debug("patch id: {}, request: {}", id, carrierPatchRequest);
-
         CreateCarrierResponse result = carriersService.patch(id, carrierPatchRequest);
-
-        log.debug("patch result: {}", result);
         return ResponseEntity.ok(result);
     }
-
 }
